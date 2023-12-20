297d63df1087b583c7d18cdbc15ef894213f8b4d
Fahmi Zainal <fahmizainal9@gmail.com>
2023-12-20 09:30:53 +0800

    feat:added files

--- practicecode.py
+++ practicecode.py
@@ -1,4 +1,14 @@
-def get_full_name(first_name,last_name):
+def get_full_name(first_name:str,last_name:str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 print(get_full_name("fahmi","zainal"))
+
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old: " + str(age)
+    return name_with_age
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old: " + str(age)
+    return name_with_age
+
